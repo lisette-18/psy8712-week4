041494b6aad8df828982662beb8da2a635c940fa
lisette-18 <horne146@umn.edu>
2024-02-11 21:40:20 -0600

    Separate columns, change to integers, and convert datetimes

--- R/week4.R
+++ R/week4.R
@@ -4,3 +4,7 @@ library(tidyverse)
 
 #Data Import
 import_tbl <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver","datadate","qs"))
+glimpse(import_tbl)
+wide_tbl <- separate(import_tbl, col = 5, into = c("q1","q2","q3","q4","q5"))
+wide_tbl[,5:9]<- sapply(wide_tbl[,5:9], as.integer)
+wide_tbl$datadate <- mdy_hms(wide_tbl$datadate) 
