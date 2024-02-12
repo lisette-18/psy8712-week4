ba833255c4a70272c056a7caee3cd82c58065467
lisette-18 <horne146@umn.edu>
2024-02-11 21:35:01 -0600

    Data Import

--- R/week4.R
+++ R/week4.R
@@ -1,3 +1,6 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+
+#Data Import
+import_tbl <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver","datadate","qs"))
